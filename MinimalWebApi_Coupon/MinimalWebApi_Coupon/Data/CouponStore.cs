d4744f427a1ab5c5e998d3af4060945ffe525ff9
gitGuedes <joaofcguedes1990@hotmail.com>
2024-09-26 22:06:25 +0100

    add a few get, post api methods

---
+++ MinimalWebApi_Coupon/MinimalWebApi_Coupon/Data/CouponStore.cs
@@ -0,0 +1,14 @@
+﻿using MinimalWebApi_Coupon.Models;
+using System.Xml.Linq;
+
+namespace MinimalWebApi_Coupon.Data
+{
+    public class CouponStore
+    {
+        public static List<Coupon> couponList = new List<Coupon>()
+        {
+            new Coupon() {Id=1,Name="Tshirt Adidas",Percent=10,IsActive=true },
+            new Coupon() {Id=2,Name="Tshirt Nike",Percent=10,IsActive=false} };
+    }
+        
+}
--- MinimalWebApi_Coupon/MinimalWebApi_Coupon/Program.cs
+++ MinimalWebApi_Coupon/MinimalWebApi_Coupon/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Mvc;
+using MinimalWebApi_Coupon.Data;
+using MinimalWebApi_Coupon.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,28 +18,47 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+//Main Code for Web Api
+{
+    app.MapGet("/api/coupon", () => Results.Ok(CouponStore.couponList)).Produces<IEnumerable<Coupon>>(200);
+
+    app.MapGet("/api/coupon/{id:int}", (int id) =>
+    {
+        var findCoupon = CouponStore.couponList.FirstOrDefault(x => x.Id == id);
+
+        return findCoupon is null ? Results.NotFound() : Results.Ok(findCoupon);
+    }).WithName("GetCoupons");
+
+    app.MapPost("/api/coupon", ([FromBody] Coupon coupon) =>
+    {
+        if (coupon.Id != 0 && string.IsNullOrEmpty(coupon.Name))
+            return Results.BadRequest("Invalid Id or Coupon Name!");
+
+        if (CouponStore.couponList.FirstOrDefault(u => u.Name.Equals(coupon.Name, StringComparison.CurrentCultureIgnoreCase)) != null)
+            return Results.BadRequest("Coupon Name already exists!");
+
+        coupon.Id = CouponStore.couponList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+        CouponStore.couponList.Add(coupon);
+
+        return Results.CreatedAtRoute("GetCoupon", new { id = coupon.Id }, coupon);
+        //return Results.Created($"/api/coupon/{coupon.Id}",coupon);
+
+    }).WithName("CreateCoupon").Produces<Coupon>(201).Produces(400);
+
+    app.MapPut("/api/coupon", () =>
+    {
+    });
+
+    app.MapDelete("/api/coupon/{id}", (int id) =>
+    {
+        bool res = CouponStore.couponList.RemoveAll(x => x.Id == id) > 0;
+
+        return res ? Results.Ok() : Results.BadRequest();
+    });
+
+}
+
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.Run();
 
 internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
